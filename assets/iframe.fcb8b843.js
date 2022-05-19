a3bc7693afccf11e52f02f3d9e67cb4c7014587c
gougm-446 <61819021+gougm-446@users.noreply.github.com>
2022-05-19 17:08:30 +0800

    Add files via upload

---
+++ assets/iframe.fcb8b843.js
@@ -0,0 +1,1 @@
+import{_ as e,d as t,o,c as r}from"./index.5efce55f.js";const _=t({setup(){return{}}}),n={id:"box"};function a(s,c,i,p,d,f){return o(),r("div",n)}var m=e(_,[["render",a]]);export{m as default};
