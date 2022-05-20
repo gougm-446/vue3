ed2dbea9f519304f51b8e4b6744c4ecc886b9cba
gougm-446 <61819021+gougm-446@users.noreply.github.com>
2022-05-20 17:06:52 +0800

    Add files via upload

---
+++ assets/iframe.f6a43f90.js
@@ -0,0 +1,1 @@
+import{_ as r,o as a,c as t,h as s}from"./index.db2dae0d.js";const c={name:"",setup(){return{src:"http://www.baidu.com"}}},n=["src"];function o(i,m,_,e,p,d){return a(),t("div",null,[s("iframe",{id:"iframe1",name:"iframe_name_1",src:e.src,width:"100px",height:"150px",frameborder:"1",style:{"margin-right":"16px"}},null,8,n)])}var l=r(c,[["render",o]]);export{l as default};
