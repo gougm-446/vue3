62a47767deb7bc1ec415d2c5db2bd1bc8ffb9ab8
gougm-446 <61819021+gougm-446@users.noreply.github.com>
2022-05-23 10:39:21 +0800

    Add files via upload

---
+++ assets/iframe.2ccbda1c.js
@@ -0,0 +1,3 @@
+import{_ as s,o as c,c as a,h as e}from"./index.4c157a9a.js";const l={name:"",setup(){return{url:"../../public/scene.html",url2:"../../public/scene2.html"}}},o=e("h1",null,"iframe1",-1),n=["src"],i=e("h1",null,"iframe2",-1),t=["src"];function d(m,f,u,r,_,p){return c(),a("div",null,[e("div",null,[o,e("iframe",{id:"iframeId",src:r.url,frameborder:"1",class:"pc iframe",scrolling:"auto"},`
+      `,8,n)]),e("div",null,[i,e("iframe",{id:"iframeId",src:r.url2,frameborder:"1",class:"pc iframe",scrolling:"auto"},`
+      `,8,t)])])}var b=s(l,[["render",d]]);export{b as default};
